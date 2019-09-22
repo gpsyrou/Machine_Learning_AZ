35a784dfa72296b374dcb1a2ae86f03abb06ce7d
gpsyrou <gspyrou@umich.edu>
2019-09-22 18:04:47 +0100

    new

---
+++ Section_5 - Multiple Linear Regression/MultipleLinearRegression.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Udemy:
+Machine Learnng A-Z
+
+
+Section 5 - Multiple Linear Regression
+
+"""
+
+import os
+os.chdir("C:\\Users\\george\\Desktop\\temp\\Multiple_Linear_Regression")
+
+# Import dataset
+import pandas as pd
+
+data = pd.read_csv("50_Startups.csv")
+
+
+
+
