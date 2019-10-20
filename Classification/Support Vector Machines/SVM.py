b632f6275c821fc75117d9fe401b542dcecde049
George Spyrou <gspyrou@umich.edu>
2019-10-20 14:11:36 +0100

    svm

--- Classification/Support Vector Machines/SVM.py
+++ Classification/Support Vector Machines/SVM.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-KNN algorithm
+Support Vector Machines
 """
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,24 +21,25 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# Fit the model
-from sklearn.neighbors import KNeighborsClassifier
-knn = KNeighborsClassifier(n_neighbors = 5,
-                           weights = 'uniform', metric = 'minkowski', p = 2, n_jobs = -1)
+# Fit the algorithm
+from sklearn.svm import SVC
 
-knn.fit(X_train, y_train)
+svc = SVC(C = 1.0, kernel = 'linear', degree = 3,
+          gamma = 'auto', probability = False, random_state = 0)
 
-# Make the predictions
-y_pred = knn.predict(X_test)
+svc.fit(X_train, y_train)
+
+y_pred = svc.predict(X_test)
+
 
 # Evaluate the results with a confusion matrix
 from sklearn.metrics import confusion_matrix
 confusion_matrix(y_true = y_test, y_pred = y_pred)
 '''
-TP: 64
-FP: 4
-FN: 3
-TN: 29
+TP: 66
+FP: 2
+FN: 8
+TN: 24
 '''
 
 # Visualise the results
@@ -46,16 +48,15 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, knn.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+plt.contourf(X1, X2, svc.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('KNN (Test set)')
+plt.title('SVM (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
-
