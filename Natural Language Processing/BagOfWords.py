43c03f24be35f8ef6cf1f8cae01ef57db4810651
George Spyrou <gspyrou@umich.edu>
2019-11-03 11:48:21 +0000

    update BagofWords.py

--- Natural Language Processing/BagOfWords.py
+++ Natural Language Processing/BagOfWords.py
@@ -13,6 +13,7 @@ data = pd.read_csv('Restaurant_Reviews.tsv', sep = '\t', header = [0])
 
 # Cleaning the text
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 import re
 
 stopWords = set(stopwords.words('english'))
@@ -23,20 +24,10 @@ data['Review'] = data['Review'].apply(lambda text: re.sub(r'[^\w\s]','',text))
 # Make all strings lowercase and split into list of words per row
 data['Review'] = data['Review'].apply(lambda text: text.lower().split())
 
-# Remove stopwords from each line
-data['Review'] = data['Review'].apply(lambda text: [x for x in text if x not in stopWords])
+# Remove stopwords from each line and perform Stemming
+ps = PorterStemmer()
+data['Review'] = data['Review'].apply(lambda text: 
+    [ps.stem(word) for word in text if word not in stopWords])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Finally make the words to join back and form a 'string' per row
+data['Review'] = data['Review'].apply(lambda ls: ' '.join(ls))
